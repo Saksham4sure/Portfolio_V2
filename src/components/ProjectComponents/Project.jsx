b2b71cc16383b0569bbd93460d9f136d0ff14acb
Saksham4sure <sakshamorig123@gmail.com>
2026-01-23 22:05:54 +0545

    New Project Section

--- src/components/ProjectComponents/Project.jsx
+++ src/components/ProjectComponents/Project.jsx
@@ -6,19 +6,21 @@ function Project({ title, img, descri, techno, link }) {
 
   return (
 
-    <Link to={link} target="_blank" className="border cursor-pointer border-[#bbbbbb] px-2 py-2 overflow-hidden relative card rounded-2xl w-full sm:w-[350px] md:w-[400px] h-[250px] md:h-[300px]">
-      <div className="flex justify-between items-center h-[20%] px-2">
-        <div className="flex items-center justify-center gap-2">
-          <h1 className="text-xl sm:text-2xl font-semibold">{title}</h1>
-        </div>
-        <div className="w-8 h-8 flex items-center justify-center border rounded-full border-[#cccccc] bg-[#fafafa]">
-          <MdArrowOutward />
+    <div className="flex flex-col md:flex-row w-full py-5 md:px-4">
+      <div className="w-full md:w-1/2 border-b border-[#bebebe] flex flex-col">
+        <h1 className="text-4xl font-semibold">{title}</h1>
+        <p className="md:text-xl md:pr-20 pt-2 leading-tight">{descri}</p>
+        <Link className="uppercase pt-6 md:pt-10 text-lg pb-2 md:text-2xl text-[#666666]" to={link}>See The Website</Link>
+        <div className="flex gap-2 capitalize md:text-xl h-full items-end pb-5">
+          {techno.map((tech) => (
+            <p>{tech}</p>
+          ))}
         </div>
       </div>
-      <div className="w-full h-[80%] overflow-hidden rounded-2xl border border-[#cccccc]">
-        <img className="w-full h-full object-cover scale-103" src={img} alt={title} />
+      <div className="md:w-1/2 overflow-hidden py-5">
+        <img className="scale-103 h-full object-cover w-full" src={img} alt={title} />
       </div>
-    </Link>
+    </div>
 
   );
 }
